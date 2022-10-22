e5b9a76f60a06a5aa21c38599ebfae0d751693cd
Sergii Gutsuliak <wheelyhog@gmail.com>
2022-10-22 10:53:42 +0200

    add tests

--- src/Wheelyhog.java
+++ src/Wheelyhog.java
@@ -12,8 +12,40 @@ public class Wheelyhog {
     }
 
     public static void main(String[] args) {
-        String[][] oldArray = {{"sasha","gmail", "123"}, {"pasha", "microsoft", "111"}, {"masha", "vk", "345"}};
-        String [] subArray = {"misha", "fb", "000"};
+//        String[][] oldArray = {{"sasha","gmail", "123"}, {"pasha", "microsoft", "111"}, {"masha", "vk", "345"}};
+//        String [] subArray = {"misha", "fb", "000"};
+        String[][] oldArray = {{"1"}, {"2", "3"}};
+        String [] subArray = {"4"};
+        System.out.println("**************");
         addNewMassiv(oldArray, subArray);
+
+
+        String [] subArray1 = {};
+        System.out.println("**************");
+        addNewMassiv(oldArray, subArray1);
+
+        String [] subArray2 = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        System.out.println("**************");
+        addNewMassiv(oldArray, subArray2);
+
+        String[][] oldArray1 = {};
+        String [] subArray3 = {};
+        System.out.println("**************");
+        addNewMassiv(oldArray1, subArray3);
+
+        String[][] oldArray2 = {};
+        String [] subArray4 = {"1", "2", "3"};
+        System.out.println("**************");
+        addNewMassiv(oldArray2, subArray4);
+
+//        addNewMassiv({{1}, {2, 3}}, {4}) -> {{1}, {2, 3}, {4}}
+//
+//        addNewMassiv({{1}, {2, 3}}, {}) -> {{1}, {2, 3}, {}}
+//
+//        addNewMassiv({{1}, {2, 3}}, {1, 2, 3, 4, 5, 6, 7, 8, 9}) -> {{1}, {2, 3}, {1, 2, 3, 4, 5, 6, 7, 8, 9}}
+//
+//        addNewMassiv({{}}, {}) -> {{}, {}}
+//
+//        addNewMassiv({{}}, {1, 2, 3}) -> {{}, {1, 2, 3}}
     }
 }
